1d849c03adf50e990c7d1d628d71d15fe46efc9c
Askebir Tadesse <askebirtadesse@gmail.com>
2026-05-23 23:29:36 +0300

    setting up some section and nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { Navbar } from "./layout/Navbar";
+import { About } from "./sections/About";
+import { Contact } from "./sections/Contact";
+import { Experience } from "./sections/Experience";
+import { Hero } from "./sections/Hero";
+import { Projects } from "./sections/Projects";
+import { Testimonials } from "./sections/Testimonials";
+
 function App() {
   return (
-    <>
-      <p>hey man</p>
-    </>
+    <div className="min-h-screen overflow-x-hidden ">
+      <Navbar />
+      <main className=" ">
+        <Hero />
+        <About />
+        <Projects />
+        <Experience />
+        <Testimonials />
+        <Contact />
+      </main>
+    </div>
   );
 }
 
---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,40 @@
+const navLink = [
+  {
+    href: "#about",
+    label: "About",
+  },
+  {
+    href: "#projects",
+    label: "Projects",
+  },
+  {
+    href: "#experience",
+    label: "Experience",
+  },
+  {
+    href: "#testimonials",
+    label: "Testimonials",
+  },
+];
+
+export const Navbar = () => {
+  return (
+    <header className="fixed top-0 left-0 right-0 bg-transparent  py-5 ">
+      <nav className="container mx-auto px-6 flex items-center justify-between ">
+        <a href="#" className="text-xl font-bold tracking-normal">
+          PM <span>.</span>
+        </a>
+        {/* Desktop Nav */}
+        <div>
+          <div>
+            {navLink.map((link, index) => (
+              <a href={link.href} key={index}>
+                {link.label}
+              </a>
+            ))}
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
