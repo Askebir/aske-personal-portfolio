445eee4795f9439e4135052a3f55c4118505c1f9
Askebir Tadesse <askebirtadesse@gmail.com>
2026-05-24 23:22:58 +0300

    desktop nav bar completion

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,3 +1,6 @@
+import { Menu } from "lucide-react";
+import { Button } from "../components/Button";
+
 const navLink = [
   {
     href: "#about",
@@ -21,19 +24,36 @@ export const Navbar = () => {
   return (
     <header className="fixed top-0 left-0 right-0 bg-transparent  py-5 ">
       <nav className="container mx-auto px-6 flex items-center justify-between ">
-        <a href="#" className="text-xl font-bold tracking-normal">
-          PM <span>.</span>
+        <a
+          href="#"
+          className="text-xl font-bold tracking-tight hover:text-primary "
+        >
+          PM <span className="text-primary">.</span>
         </a>
         {/* Desktop Nav */}
-        <div>
-          <div>
+        <div className=" hidden md:flex items-center gap-1">
+          <div className="glass  rounded-full px-2 py-1 flex items-center gap-1 ">
             {navLink.map((link, index) => (
-              <a href={link.href} key={index}>
+              <a
+                href={link.href}
+                key={index}
+                className="px-4 py-2 text-muted-foreground  "
+              >
                 {link.label}
               </a>
             ))}
           </div>
         </div>
+        {/* CTA Button */}
+        <div className="hidden md:block">
+          <Button size="sm">Contact Me</Button>
+        </div>
+
+        {/* Mobile menu button */}
+
+        <button className="md:hidden p-2 text-foreground ">
+          <Menu size={24} />
+        </button>
       </nav>
     </header>
   );
