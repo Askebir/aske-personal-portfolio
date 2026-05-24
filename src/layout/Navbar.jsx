dbb7142c844cc8f4db80aac1c015ec6b4856b662
Askebir Tadesse <askebirtadesse@gmail.com>
2026-05-24 23:47:20 +0300

    Mobile menu

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,5 +1,6 @@
-import { Menu } from "lucide-react";
+import { Menu, X } from "lucide-react";
 import { Button } from "../components/Button";
+import { useState } from "react";
 
 const navLink = [
   {
@@ -21,6 +22,8 @@ const navLink = [
 ];
 
 export const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
   return (
     <header className="fixed top-0 left-0 right-0 bg-transparent  py-5 ">
       <nav className="container mx-auto px-6 flex items-center justify-between ">
@@ -37,7 +40,7 @@ export const Navbar = () => {
               <a
                 href={link.href}
                 key={index}
-                className="px-4 py-2 text-muted-foreground  "
+                className="px-4 py-2 text-muted-foreground hover:text-foreground rounded-full hover:bg-surface "
               >
                 {link.label}
               </a>
@@ -51,10 +54,30 @@ export const Navbar = () => {
 
         {/* Mobile menu button */}
 
-        <button className="md:hidden p-2 text-foreground ">
-          <Menu size={24} />
+        <button
+          className="md:hidden p-2 text-foreground  cursor-pointer"
+          onClick={() => setIsMobileMenuOpen((prev) => !prev)}
+        >
+          {isMobileMenuOpen ? <X size={24} /> : <Menu size={24} />}
         </button>
       </nav>
+      {/* Mobile Menu */}
+      {isMobileMenuOpen && (
+        <div className="md:hidden glass-strong ">
+          <div className="container mx-auto px-6 py-6 flex flex-col gap-4">
+            {navLink.map((link, index) => (
+              <a
+                href={link.href}
+                key={index}
+                className="text-lg  text-muted-foreground hover:text-foreground py-2   "
+              >
+                {link.label}
+              </a>
+            ))}
+            <Button>Contact Me</Button>
+          </div>
+        </div>
+      )}
     </header>
   );
 };
