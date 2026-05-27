3dcd94443fa8a9f90b8ba6c998c6e2b639001973
Askebir Tadesse <askebirtadesse@gmail.com>
2026-05-28 00:28:48 +0300

    experience section complated

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,6 +1,6 @@
 const experiences = [
   {
-    pariod: "2022 - present",
+    period: "2022 - present",
     role: "Senior Fronted Engineer",
     company: "Tech  Innovators Inc",
     description:
@@ -9,7 +9,7 @@ const experiences = [
     current: true,
   },
   {
-    pariod: "2020 - 2022",
+    period: "2020 - 2022",
     role: "Frontend Engineer",
     company: "Digital Solutions Co.",
     description:
@@ -18,7 +18,7 @@ const experiences = [
     current: false,
   },
   {
-    pariod: "2019 - 2020",
+    period: "2019 - 2020",
     role: "Junior Developer",
     company: "StartUp Labs",
     description:
@@ -27,7 +27,7 @@ const experiences = [
     current: false,
   },
   {
-    pariod: "2018 - 2019",
+    period: "2018 - 2019",
     role: "Freelance Developer",
     company: "Self-Employed",
     description:
@@ -40,7 +40,7 @@ const experiences = [
 export const Experience = () => {
   return (
     <section id="experience" className="py-32  relative overflow-hidden">
-      <div className="absolute top-1/2 left-1/4 w-96 h-96 bg-primary/5 rounded-full blur-3xl -translate-x-1/2"></div>
+      <div className="absolute top-1/2 left-1/2 w-96 h-96 bg-primary/30 rounded-full blur-3xl -translate-x-1/2"></div>
       <div className="container mx-auto px-6 relative z-10">
         {/* Section Header */}
         <div className="max-w-3xl mb-16">
@@ -60,7 +60,55 @@ export const Experience = () => {
         </div>
         {/* Timeline */}
         <div className="relative">
-          <div className="timeline-glow absolute left-0 md:left-1/2 top-0 bottom-0 w-[2px] bg=gradient-to-b from-primary/70 via-primary/30 to-transparent md:-translate-x-1/2 shadow-[0_0_25px_rgba(32,178,160,0.8)]" />
+          <div className="timeline-glow absolute left-0 md:left-1/2 top-0  bottom-0 w-[2px] bg-linear-to-b from-primary/70 via-primary/30 to-transparent md:-translate-x-1/2 shadow-[0_0_25px_rgba(32,178,160,0.8)]" />
+          {/* EXPERIENCE ITMES */}
+
+          <div className="space-y-12">
+            {experiences.map((exp, index) => (
+              <div
+                key={index}
+                className="relative grid md:grid-cols-2 gap-8 aimate-fade-in"
+                style={{ animationDelay: `${(index + 1) * 150}ms` }}
+              >
+                {/* Timeline Dot */}
+                <div className="absolute left-0 md:left-1/2 top-0 w-3 h-3 bg-primary rounded-full -translate-x-1/2 ring-4 ring-background z-10">
+                  {exp.current && (
+                    <span className="absolute inset-0 rounded-full bg-primary animate-ping opacity-75" />
+                  )}
+                </div>
+
+                {/* Content */}
+                <div
+                  className={`pl-8 md:pl-0 ${index % 2 === 0 ? "md:pr-16 md:text-right" : "md:col-start-2 md:pl-16"}`}
+                >
+                  <div
+                    className={`glass p-6 rounded-2xl border border-primary/30 hover:border-primary/50 transition-all duration-500`}
+                  >
+                    <span className="text-sm text-primary font-medium">
+                      {exp.period}
+                    </span>
+                    <h3 className="text-xl font-semibold mt-2">{exp.role}</h3>
+                    <p className="text-muted-foreground">{exp.company}</p>
+                    <p className="text-sm text-muted-foreground mt-4">
+                      {exp.description}
+                    </p>
+                    <div
+                      className={`flex flex-wrap gap-2 mt-4 ${index % 2 === 0 ? "md:justify-end" : ""} `}
+                    >
+                      {exp.technologies.map((tech, techindx) => (
+                        <span
+                          key={techindx}
+                          className="px-3 py-1 bg-serface text-xs rounded-full text-muted-foreground"
+                        >
+                          {tech}
+                        </span>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
