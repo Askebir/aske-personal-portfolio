4184ae0a42ef6422b968a834b96af9cb819b1ce4
Askebir Tadesse <askebirtadesse@gmail.com>
2026-05-29 22:42:48 +0300

    contact act section ui

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,3 +1,117 @@
+import {
+  Mail,
+  Phone,
+  MapPin,
+  Send,
+  CheckCircle,
+  AlertCircle,
+} from "lucide-react";
+import { Button } from "@/components/Button";
+import { useState } from "react";
+// import emailjs from "@emailjs/browser";
+
+const contactInfo = [
+  {
+    icon: Mail,
+    label: "Email",
+    value: "pedro@example.com",
+    href: "mailto:pedro@example.com",
+  },
+  {
+    icon: Phone,
+    label: "Phone",
+    value: "+1 (555) 123-4567",
+    href: "tel:+15551234567",
+  },
+  {
+    icon: MapPin,
+    label: "Location",
+    value: "San Francisco, CA",
+    href: "#",
+  },
+];
+
 export const Contact = () => {
-  return <section></section>;
+  return (
+    <section className="py-32 relative overflow-hidden">
+      <div className="absolute top-0 left-0 w-full h-full ">
+        <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-primary/5 rounded-full blur-3xl"></div>
+        <div className="absolute buttom-1/4 right-1/4 w-64 h-64 bg-highlight/5 blur-3xl "></div>
+      </div>
+      <div className="container mx-auto px-6 relative z-10">
+        {/* section of header */}
+        <div className="text-center max-w-3xl mx-auto mb-16">
+          <span>Get In Touch</span>
+          <h2 className=" text-4xl md:text-5xl font-bold mt-4 mb-6 aniamte-fade-in text-blue-700">
+            Let's build{" "}
+            <span className="font-serif italic font-normal text-white">
+              something great.
+            </span>
+          </h2>
+          <p className="text-muted-foreground animate-fade-in animation-delay">
+            Have a project in mind? I'd love to hear about it. Send me a message
+            and let's discuss how we can work together
+          </p>
+        </div>
+
+        <div className="grid lg:grid-cols-2 gap-12 max-w-5xl mx-auto">
+          <div className="glass p-8 rounded-3xl border border-primary/30 animate-fade-in animate-delay-300 ">
+            <form action="" className="space-y-6">
+              <div>
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-medium mb-2"
+                >
+                  Name
+                </label>
+                <input
+                  id="name"
+                  type="text"
+                  required
+                  placeholder="Your name"
+                  className="w-full px-4 py-3 bg-surface rounded-2xl border border-border foucs:ring-1 outline-none focus:ring-primary focus:border-primary "
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium mb-2"
+                >
+                  Email
+                </label>
+                <input
+                  id="email"
+                  type="emial"
+                  required
+                  placeholder="Your@email.com"
+                  className="w-full px-4 py-3 bg-surface rounded-2xl border border-border foucs:ring-1 outline-none focus:ring-primary focus:border-primary "
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="message"
+                  className="block text-sm font-medium mb-2"
+                >
+                  Message
+                </label>
+                <textarea
+                  id="message"
+                  type="text"
+                  rows={5}
+                  required
+                  placeholder="Your message..."
+                  className=" resize-none w-full px-4 py-3 bg-surface transition-all rounded-2xl border border-border foucs:ring-1 outline-none focus:ring-primary focus:border-primary "
+                />
+              </div>
+              <Button className="w-full  " type="submit" size="lg">
+                Send Message <Send />
+              </Button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
